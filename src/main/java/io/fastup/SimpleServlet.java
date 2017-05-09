4c204e03439d4b184f18d758a651aa2675333a51
Sachin <sdole@genvega.com>
2017-05-09 15:46:38 -0500

    call customer app from api app

--- src/main/java/io/fastup/SimpleServlet.java
+++ src/main/java/io/fastup/SimpleServlet.java
@@ -3,14 +3,29 @@ package io.fastup;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URL;
 
 public class SimpleServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("application/json");
         PrintWriter out = res.getWriter();
         out.println("{'message':'hello world FROM API TIER','version':" + System.getenv("FastupVersion") + "}");
+        URL customerApp = new URL("https://customer.spinci.fastup.io/customer-app-1.0-SNAPSHOT/service");
+        BufferedReader in = new BufferedReader(new InputStreamReader(customerApp.openStream()));
+        String inputLine;
+        StringBuilder resString = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null)
+            resString.append(inputLine);
+        out.println(
+                "{'message':'hello world FROM API TIER','version':" + System.getenv("FastupVersion") + "," +
+                        "'customer_message':'" + resString.toString() + "'}"
+        );
+        in.close();
         out.flush();
         out.close();
     }
